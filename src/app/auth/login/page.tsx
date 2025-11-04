9ce55d81566937f4077b58c5bc336975eea7d2f2
abuyusufrony <abuyusufrony@gmail.com>
2025-11-05 00:01:17 +0600

    login-picture-added

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,5 +1,40 @@
+import Image from "next/image";
+
 const LoginPage = () => {
-  return <h1>login</h1>;
+  return (
+    <div>
+      <h1>login</h1>
+      <div className="flex justify-center">
+        <div>
+          <Image className="top-1  left-1.5"
+            src="/Frame 2147226025.png"
+            alt="example"
+            width={748}
+            height={1016}
+          />
+        </div>
+        <div>
+          <div className="flex justify-center items-center">
+
+            <div className="">
+              <Image className=" bg-blue-500 "
+                src="/vector.png"
+                alt="example"
+                width={50}
+                height={50}
+              />
+            </div>
+            <div>
+              <h5>Ehen Tours</h5>
+            </div>
+          </div>
+          <h2 className="text-3xl font-bold">
+            Welcome To Travel
+          </h2>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
