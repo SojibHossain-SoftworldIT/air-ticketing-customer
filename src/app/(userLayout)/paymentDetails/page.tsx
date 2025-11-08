52995cf336f96aaa0ce492d3ab2a9f14d9d6081f
abuyusufrony <abuyusufrony@gmail.com>
2025-11-09 00:01:02 +0600

    hero-added

---
+++ src/app/(userLayout)/paymentDetails/page.tsx
@@ -0,0 +1,11 @@
+import PaymentDetails from '@/components/modules/paymentDetails/main/paymentDetails';
+import React from 'react';
+
+
+const page = () => {
+    return (
+        <PaymentDetails></PaymentDetails>
+    );
+};
+
+export default page;
---
+++ src/components/modules/paymentDetails/main/PaymentDetails.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import TobHero from '../../PersonalDetails/TobHero';
+import ContainerPersonalDetails from '@/components/common/ContainerPersonalDetails/main/ContainerPersonalDetails';
+
+const PaymentDetails = () => {
+    return (
+        <div>
+            <TobHero></TobHero>
+            <ContainerPersonalDetails></ContainerPersonalDetails>
+        </div>
+    );
+};
+
+export default PaymentDetails;
